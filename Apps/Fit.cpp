cce99d2649b2183185f520503a96d6d43228db25
Luke Pickering <luketpickering@gmail.com>
2026-03-04 15:03:49 +0000

    moves some analysis-specific app modifications out of the way

--- Apps/Fit.cpp
+++ Apps/Fit.cpp
@@ -35,17 +35,12 @@ int main(int argc, char * argv[]) {
   auto OutputFile = std::unique_ptr<TFile>(TFile::Open(OutputFileName.c_str(), "RECREATE"));
   OutputFile->cd();
 
-
-  //For fake data study
-  xsec->ToggleFixParameter("NuWroFakeDataWeight"); //take fake data systematic parameters and fix them to 1.0
-  xsec->SetPar(xsec->GetParIndex("NuWroFakeDataWeight"), 1);
-
   for (unsigned sample_i = 0 ; sample_i < DUNEPdfs.size() ; ++sample_i) {
-
+    
     std::string name = DUNEPdfs[sample_i]->GetTitle();
     sample_names.push_back(name);
     TString NameTString = TString(name.c_str());
-
+    
     DUNEPdfs[sample_i] -> Reweight();
     PredictionHistograms.push_back(static_cast<TH1*>(DUNEPdfs[sample_i]->GetMCHist(DUNEPdfs[sample_i]->GetNDim())->Clone(NameTString+"_unosc")));
 
@@ -54,26 +49,22 @@ int main(int argc, char * argv[]) {
     } else if (DUNEPdfs[sample_i]->GetNDim() == 2){
       DUNEPdfs[sample_i]->AddData(static_cast<TH2D*>(PredictionHistograms[sample_i]));
     }
-
+    
     else {
-      MACH3LOG_ERROR("Unsupported number of dimensions > 2 - Quitting");
+      MACH3LOG_ERROR("Unsupported number of dimensions > 2 - Quitting"); 
       throw MaCh3Exception(__FILE__ , __LINE__ );
     }
-
-
+    
+    
   }
-
-  //Now we have made the data histograms we need to make sure that parameters are all reset to their prior values...
-  xsec->SetPar(xsec->GetParIndex("NuWroFakeDataWeight"), 0);
-  xsec->ToggleFixParameter("NuWroFakeDataWeight");
-
-  //Now print out some event rates, we'll make a nice latex table at some point
+  
+  //Now print out some event rates, we'll make a nice latex table at some point 
   for (unsigned iPDF = 0; iPDF < DUNEPdfs.size() ; ++iPDF) {
     MACH3LOG_INFO("Integrals of nominal hists: ");
     MACH3LOG_INFO("{} : {}",sample_names[iPDF].c_str(),PredictionHistograms[iPDF]->Integral());
     MACH3LOG_INFO("--------------");
   }
-
+  
   //###########################################################################################################
   //MCMC
 
@@ -81,26 +72,11 @@ int main(int argc, char * argv[]) {
 
   bool StartFromPreviousChain = GetFromManager(FitManager->raw()["General"]["StartFromPos"], false);
   //Start chain from random position unless continuing a chain
-  // if(!StartFromPreviousChain){
-  //   if (!GetFromManager(FitManager->raw()["General"]["StatOnly"], false)) {
-  //     xsec->ThrowParameters();
-  //   }
-  // }
-  if (!StartFromPreviousChain) {
-  if (!GetFromManager(FitManager->raw()["General"]["StatOnly"], false)) {
-
-    const int nPars = xsec->GetNParameters();
-    std::vector<double> startPars(nPars);
-
-    for (int i = 0; i < nPars; ++i) {
-      startPars[i] = xsec->GetParInit(i);  // this is 1.0
+  if(!StartFromPreviousChain){
+    if (!GetFromManager(FitManager->raw()["General"]["StatOnly"], false)) {
+      xsec->ThrowParameters();
     }
-
-    xsec->SetParameters(startPars);
-    MACH3LOG_INFO("Initialized xsec parameters to prior values");
   }
-}
-
 
   //Add systematic objects
   MaCh3Fitter->AddSystObj(xsec);
@@ -110,83 +86,13 @@ int main(int argc, char * argv[]) {
     MACH3LOG_INFO("MCMC getting starting position from: {}",PreviousChainPath);
     MaCh3Fitter->StartFromPreviousFit(PreviousChainPath);
   }
-
+  
   //Add samples
   for(auto Sample : DUNEPdfs){
     MaCh3Fitter->AddSampleHandler(Sample);
   }
 
-    //stuff added from Liban
-
-  std::string throwmatrixfilename = GetFromManager<std::string>(FitManager->raw()["General"]["ThrowMatrixFile"], "");
-  std::string throwmatrixname = GetFromManager<std::string>(FitManager->raw()["General"]["ThrowMatrixName"], "");
-  if (throwmatrixfilename == "") {
-    MACH3LOG_INFO("No throw matrix file specified, will throw from covariance matrix.");
-  }
-  else {
-    TFile *throwmatrixfile = new TFile(throwmatrixfilename.c_str());
-    if (throwmatrixfile->IsZombie()) {
-      MACH3LOG_ERROR("Couldn't find {}", throwmatrixfilename);
-      throw MaCh3Exception(__FILE__ , __LINE__ );
-    }
-    // MACH3LOG_INFO("Found throw matrix file {}.", throwmatrixfilename);
-    // TMatrixDSym *throwmatrix = throwmatrixfile->Get<TMatrixDSym>(throwmatrixname.c_str());
-    // if (!throwmatrix) {
-    //   MACH3LOG_ERROR("Couldn't find throw matrix {} in file {}", throwmatrixname, throwmatrixfilename);
-    //   throw MaCh3Exception(__FILE__ , __LINE__ );
-    // }
-    // Reset individual step scales to 1.0
-    xsec->ResetIndivStepScale();
-    // Keep global step scale flexible, but it should be 1
-    double globalStepScale = FitManager->raw()["General"]["Systematics"]["XsecStepScale"].as<double>();
-    if (globalStepScale != 1.0) {
-      MACH3LOG_WARN("Global step scale is not 1.0, it is set to {}. This may cause issues when using adapted throw matrix.", globalStepScale);
-    }
-    xsec->SetStepScale(globalStepScale);
-    MACH3LOG_WARN("I have set all the individual step scales to 1.0 since we are using an external throw matrix");
-    //xsec->SetThrowMatrix(throwmatrix);
-    TMatrixTSym<double>* throwmatrix =
-    dynamic_cast<TMatrixTSym<double>*>(
-        throwmatrixfile->Get(throwmatrixname.c_str())
-    );
-
-    // if (!throwmatrix) {
-    //   MACH3LOG_ERROR("Throw matrix is not TMatrixTSym<double>");
-    //   throw MaCh3Exception(__FILE__, __LINE__);
-    // }
-    xsec->SetThrowMatrix(throwmatrix);
-    MACH3LOG_INFO("Set throw matrix from file {} with name {}",
-                  throwmatrixfilename, throwmatrixname);
-    // Print the throw matrix diagonals
-    for (int i = 0; i < throwmatrix->GetNrows(); i++) {
-      std::cout << (*throwmatrix)(i, i) << " ";
-    }
-    std::cout << std::endl;
-
-    TVectorD eig;
-    throwmatrix->EigenVectors(eig);
-
-    double min = 1e30, max = 0;
-    for (int i = 0; i < eig.GetNrows(); ++i) {
-      min = std::min(min, eig[i]);
-      max = std::max(max, eig[i]);
-    }
-
-    std::cout << "Eigenvalues: min=" << min
-              << " max=" << max << std::endl;
-
-
-
-
-
-  }
-
-
-
-  //
-  //bool StartFromPreviousChain = GetFromManager(FitManager->raw()["General"]["StartFromPos"], false);
-
-
+  
   //Run fit
   MaCh3Fitter->RunMCMC();
 
