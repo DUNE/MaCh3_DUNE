7fc07d95903f2e69d6cafde1229eadc56be4ea51
Luke Pickering <luketpickering@gmail.com>
2026-03-06 21:00:34 +0000

    missed saving a file...

--- Samples/BeamOffAxis/Systematics.h
+++ Samples/BeamOffAxis/Systematics.h
@@ -22,17 +22,6 @@ void MuonEnergyResolution(const double *par_val, EventInfo &ev) ;
 void NeutronEnergyResolution(const double *par_val, EventInfo &ev);
 void ChargedHadronEnergyResolution(const double *par_val, EventInfo &ev);
 
-inline auto GetFluxFocussingWeighter(int focussing_par) {
-  return [=](double const *par_val, EventInfo const &ev) {
-    return 1 + (*par_val) * ev.syst.flux.focussing_ratio[focussing_par];
-  };
-}
-inline auto GetFluxHadronProductionWeighter(int hadprod_par) {
-  return [=](double const *par_val, EventInfo const &ev) {
-    return 1 + (*par_val) * ev.syst.flux.hadprod_ratio[hadprod_par];
-  };
-}
-
 std::pair<std::vector<float>, std::vector<float>>
 GetFluxVariationRatios(int nu_pdg, double enu_true_GeV, double off_axis_pos_m,
                        bool is_numode);
