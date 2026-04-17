86178003a4323be60a8151d1d8a4293355d4f3b2
Henry Wallace <henryi@fc30669.int.fir.alliancecan.ca>
2026-04-17 08:11:16 -0700

    Fix example

--- python/pyMaCh3DUNE_Example.py
+++ python/pyMaCh3DUNE_Example.py
@@ -1,8 +1,10 @@
 
-from dataclasses import dataclass
 from pathlib import Path
 import os
 from typing import TypedDict, Literal
+
+from matplotlib import pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
 
 from pyMaCh3_DUNE import parameters
 from pyMaCh3_DUNE import samples
@@ -72,6 +74,27 @@ def setup_samples(mach3_path: Path, parameters: parameters.ParameterHandlerGener
     
     return [make_sample(s.get('path'), s.get('type'), parameters) for s in sample_configs]    
     
+def plot_mc_hist(sample_handlers: list[samples.SampleHandlerBase], output_file: Path):
+    '''
+    Plots the MC histograms    
+    '''
+
+    output_file.parent.mkdir(exist_ok=True, parents=True)
+    
+    with PdfPages(str(output_file)) as pdf:
+        for handler in sample_handlers:
+            handler.reweight()
+            for n in range(handler.get_n_samples()):
+                mc_pred, bin_edges_x, bin_edges_y = handler.get_mc_hist(n)
+                            
+                # 1D hist ()
+
+                if not len(bin_edges_y):
+                    plt.hist(bin_edges_x[:-1], bins=bin_edges_x, weights=mc_pred, histtype="step")
+                else:
+                    plt.hist2d(bin_edges_x[:-1], bin_edges_y[:-1], [bin_edges_x, bin_edges_y], weights=mc_pred)
+                pdf.savefig()
+                plt.close()
     
 if __name__=="__main__":
     # We can set up MaCh3 in a few ways!
@@ -84,3 +107,4 @@ if __name__=="__main__":
 
     parameter_handler = setup_parameters(mach3_path)
     sample_handlers = setup_samples(mach3_path, parameter_handler)
+    plot_mc_hist(sample_handlers, mach3_path/"mc_hist_example.pdf")
