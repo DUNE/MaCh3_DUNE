a6dedf52159e3651b1e3c5922bb2d3d9454e1e31
Alistair Carney <alistair.carney.2021@live.rhul.ac.uk>
2026-04-20 06:08:34 -0700

    Updates to work with reduced parameter set and make version of posterior predictive

--- HistOverlap.c
+++ HistOverlap.c
@@ -4,9 +4,11 @@
 #include "TBrowser.h"
 #include "TH2.h"
 #include "TRandom.h"
+#include <fmt/core.h>
+
 
 void overlay() { // Create a script to print two histograms on same canvas
-    TFile* OscMF = TFile::Open("OscMFNoNC.root"); // Load in histograms
+    TFile* OscMF = TFile::Open("PostPredPMNSv2.root"); // Load in histograms
     TFile* OscPMNS = TFile::Open("OscPMNSNoNC.root");
 
     TIter next(OscMF->GetListOfKeys()); // Producing list of keys (histograms) from file
@@ -32,6 +34,55 @@ void overlay() { // Create a script to print two histograms on same canvas
         leg->AddEntry(HistoUnosc,"PMNS","l");
         leg->Draw();
 
-        c->SaveAs(Form("Histo_%s.png", key->GetName())); // Saving and creating title
+        c->SaveAs("PostPred.png"); // Saving and creating title
     }
+}
+
+
+void PostPred() { // Create a script to print two histograms on same canvas
+    TFile* OscPMNS = TFile::Open("PostPredPMNSv2.root"); // Load in histograms
+    TFile* OscMF = TFile::Open("PostPredFHCnumuv2.root");
+    TFile* OscData = TFile::Open("OscPMNSNoNC.root");
+
+    auto HistoPMNS = OscPMNS->Get<TH1D>("Predictive/FD_FHC_numu/FD_FHC_numu_mc_PostPred"); // PostPredPMNSv2
+    auto HistoMF = OscMF->Get<TH1D>("Predictive/FD_FHC_numu/FD_FHC_numu_mc_PostPred");
+    auto HistoData = OscData->Get<TH1D>("hRecoNeutrinoEnergyFHC_numu");
+
+    HistoPMNS->SetLineColor(kRed); // Cosmetics for PMNS model histogram
+    HistoPMNS->SetLineWidth(2);
+    HistoPMNS->SetStats(0);
+
+    HistoMF->SetLineColor(kBlue); // Cosmetics for Model Free model histogram
+    HistoMF->SetLineWidth(2);
+    HistoMF->SetStats(0);
+
+    HistoData->SetLineColor(kBlack); // Cosmetics for data histogram
+    HistoData->SetLineWidth(2);
+    HistoData->SetLineStyle(2);
+    HistoData->SetStats(0);
+
+    TCanvas *c = new TCanvas("c","Overlay"); // Creating canvas and plotting, may want to change order of Osc/Unosc based on visuals 
+    HistoMF->Draw("HIST E0");
+    HistoMF->SetTitle("Posterior Predictive FHC_numu");
+    HistoPMNS->Draw("HIST SAME E0");
+    HistoData->Draw("HIST SAME");
+
+
+    auto leg = new TLegend(0.5, 0.5, 0.88, 0.88); // Creating the legend
+    leg->SetMargin(0.4);
+    leg->AddEntry(HistoMF,fmt::format("Model Free - #int = {:.2f}", HistoMF->Integral()).c_str(),"lpf");
+    leg->AddEntry(HistoPMNS,fmt::format("PMNS - #int = {:.2f}", HistoPMNS->Integral()).c_str(),"lpf");
+    leg->AddEntry(HistoData,fmt::format("Data - #int = {:.2f}", HistoData->Integral()).c_str(),"lpf");
+    leg->Draw();
+
+    c->SaveAs("PostPred.png"); // Saving and creating title
+
+    // int NumBins = HistoModel->GetNbinsX(); // Find number of bins (energy normalisation parameters) for each histogram
+    // for(int j = 1; j <= NumBins; j++) {
+    //     double BinSizeModel = HistoModel->GetBinContent(j); // Find number of events for each energy parameter
+    //     double BinSizeData = HistoData->GetBinContent(j);
+    //     double Param;
+    //     Param = BinSizeModel / BinSizeData;
+    //     std::cout << Param << "\n";
+    // }
 }
--- ParamProbCol.c
+++ ParamProbCol.c
@@ -12,17 +12,19 @@
 #include "TH2.h"
 #include "TRandom.h"
 
+// ############## CHANGE AS NEEDED ############## //
 const int NParams = 1920; // Number of energy normalisation parameters
-const int NEBins = 500; // Number of bins in the colour plots
+const int NEBins = 20; // Number of probability bins in the colour plots
+std::vector<std::string> Sample = {"FHC_numu", "FHC_nue", "RHC_numu", "RHC_nue"}; // Creating vector of sample titles
+//std::vector<std::string> Sample = {"FHC_numu"};
 
+// ############## FIXED PARAMS/VALUES ############## //
+const int EBinsChan = 40; // Number of energy bins per channel
 std::vector<double> BinStart = {0.0,0.25,0.5,0.75,1.0,1.25,1.5,1.75,2.0,2.25,2.5,2.75,3.0,3.25,3.5,3.75,4.0,4.25,4.5,4.75,5.0,5.25,5.5,5.75,6.0,6.25,6.5,6.75,7.0,7.25,7.5,7.75,8.0,8.25,8.5,8.75,9.0,9.25,9.5,9.75,10.0};
 std::vector<double> BinMid = {0.125,0.375,0.625,0.875,1.125,1.375,1.625,1.875,2.125,2.375,2.625,2.875,3.125,3.375,3.625,3.875,4.125,4.375,4.625,4.875,5.125,5.375,5.625,5.875,6.125,6.375,6.625,6.875,7.125,7.375,7.625,7.875,8.125,8.375,8.625,8.875,9.125,9.375,9.625,9.875};
-
 std::vector<double> NormParams(NParams, 0.0); // Creating empty vector to store all energy normalisation parameters
-
 std::map<int, std::string> NuType = {{12, "nue"}, {14, "numu"}, {16, "nutau"}, {-12, "nuebar"}, {-14, "numubar"}, {-16, "nutaubar"}}; // Creating map between neutrino "value" and "name"
 std::vector<std::pair<int,int>> OscChan = {{14, 14}, {12, 12}, {-14, -14}, {-12, -12}, {14, 12}, {-14, -12}, {12, 14}, {-12, -14}, {14, 16}, {12, 16}, {-14, -16}, {-12, -16}}; // Creating all oscillation channels as pairs of neutrino "value"
-std::vector<std::string> Sample = {"FHC_numu", "FHC_nue", "RHC_numu", "RHC_nue"}; // Creating vector of sample titles
 std::vector<TH2D*> Hists; // Creating empty vector to store histograms
 
 void NormaliseAndPlot(TH2D* h, TString HistName) // Create a script to column normalise and then plot a given histogram with a given name
@@ -55,7 +57,7 @@ void NormaliseAndPlot(TH2D* h, TString HistName) // Create a script to column no
     auto c = new TCanvas(); // Create a canvas to plot
     c->Draw(); // Draw the canvas
     c->cd();
-    c->SetLogy(); // Set log scale
+    //c->SetLogy(); // Set log scale
     h->Draw("COLZ"); // Draw colour scale histogram
     h->GetXaxis()->SetTitle("Energy (GeV)"); // Setting axes properties
     h->GetYaxis()->SetTitle("Oscillation Probability");
@@ -64,7 +66,7 @@ void NormaliseAndPlot(TH2D* h, TString HistName) // Create a script to column no
     h->SetStats(0); // Cosmetics of plot
     c->SetRightMargin(0.15);
     c->Update();
-    c->Print(HistName + "OscProbCol.pdf"); // Print colour histogram to pdf
+    c->Print(HistName + "_OscProbCol.pdf"); // Print colour histogram to pdf
 }
 
 void ChanColPlot(TString file_name) // Create a script to produce colour plots for each oscillation channel from a given root file
@@ -88,16 +90,16 @@ void ChanColPlot(TString file_name) // Create a script to produce colour plots f
             std::string unosc_name = NuType[chan.first]; // Unoscillated neutrino type is first value in pairing
             std::string osc_name   = NuType[chan.second]; // Oscillated neutrino type is second value in pairing
             std::string HistName = sample + "_" + unosc_name + "_" + osc_name; // Create name for each histogram using sample name and oscillation channel
-            Hists.push_back(new TH2D(HistName.c_str(), ("Oscillation Probability for " + HistName).c_str(), NParams/48, BinStart.data(), NEBins, 0.0, 1.0)); // Create histogram for each channel in each sample
+            Hists.push_back(new TH2D(HistName.c_str(), ("Oscillation Probability for " + HistName).c_str(), EBinsChan, BinStart.data(), NEBins, 0.0, 0.1)); // Create histogram for each channel in each sample
         }
     }
 
     Long64_t nentries = t1->GetEntries(); // Get number of entries (steps) in given file
-    for (Long64_t i=8000000; i<nentries; i++) { // Start at some burn-in cut, run for rest of entries
+    for (Long64_t i=4000000; i<nentries; i++) { // Start at some burn-in cut, run for rest of entries
         t1->GetEntry(i); // Get the relevant information
         for(int p = 0; p < NParams; p++){ // For each parameter
-            int k = p % (NParams/48); // Find the value of param index modulo 40 (each value corresponds to a different energy bin (40))
-            int g = p / (NParams/48); // Find the integer division of param index / 48 (each value corresponds to a different histogram (48))
+            int k = p % EBinsChan; // Find the value of param index modulo 40 (each value corresponds to a different energy bin (40))
+            int g = p / EBinsChan; // Find the integer division of param index / 48 (each value corresponds to a different histogram (48))
             Hists[g]->Fill(BinMid[k], NormParams[p]); // Fill each histogram with the relevant parameter from the desired energy bin
         }
     }
@@ -106,5 +108,8 @@ void ChanColPlot(TString file_name) // Create a script to produce colour plots f
     //     NormaliseAndPlot(h, h->GetName()); // Normalise by column and plot, getting names as required
     // }
 
-    NormaliseAndPlot(Hists[0], Hists[0]->GetName()); // Plot an individual colour map
+    //NormaliseAndPlot(Hists[0], Hists[0]->GetName()); // FHC_numu: numu -> numu
+    NormaliseAndPlot(Hists[16], Hists[16]->GetName()); // FHC_nue: numu -> nue 
+    //NormaliseAndPlot(Hists[26], Hists[26]->GetName()); // RHC_numu: numubar -> numubar
+    NormaliseAndPlot(Hists[41], Hists[41]->GetName()); // RHC_nue: numubar -> nuebar
 }
