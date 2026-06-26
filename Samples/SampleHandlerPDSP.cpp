0060ba9e916a1a972e2f079452c4033090e74d23
Dennis Lindebaum <dennis.lindebaum@bristol.ac.uk>
2026-06-26 16:03:54 +0000

    Fix type in true KE init

--- Samples/SampleHandlerPDSP.cpp
+++ Samples/SampleHandlerPDSP.cpp
@@ -170,8 +170,8 @@ int SampleHandlerPDSP::SetupExperimentMC() {
       bool true_pip;
       bool true_decay;
 
-      _data->SetBranchStatus("KE_init_reco", true);
-      _data->SetBranchAddress("KE_init_reco", &trueKEIni);
+      _data->SetBranchStatus("KE_init_true", true);
+      _data->SetBranchAddress("KE_init_true", &trueKEIni);
 
       _data->SetBranchStatus("KE_int_true", true);
       _data->SetBranchAddress("KE_int_true", &trueKEInt);
