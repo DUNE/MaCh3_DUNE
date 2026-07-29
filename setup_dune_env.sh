4ac121586a041f314f44ef27170c55e96df58fa0
Daniel Barrow <danbarrow257@gmail.com>
2026-07-29 09:03:37 -0500

    Add VDT info into the environment setup

--- setup_dune_env.sh
+++ setup_dune_env.sh
@@ -4,3 +4,6 @@ spack load cmake
 spack load gcc@12.2.0
 
 source /cvmfs/larsoft.opensciencegrid.org/spack-packages/opt/spack/linux-almalinux9-x86_64_v2/gcc-12.2.0/root-6.28.12-sfwfmqorvxttrxgfrfhoq5kplou2pddd/bin/thisroot.sh
+
+spack load vdt
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/cvmfs/larsoft.opensciencegrid.org/spack-packages/opt/spack/linux-almalinux9-x86_64_v2/gcc-11.4.1/vdt-0.4.4-kyttjsy6ilqquzjsoi2t2pwiwdoizmnx/lib/
